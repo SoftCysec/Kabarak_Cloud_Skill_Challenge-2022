9e2745361ee2b8ad38474cd52cda426c06d9dea1
SoftCysec <softengdedan@gmail.com>
2022-11-07 22:08:10 +0300

    Javascript Decision Making

---
+++ Make Decisions with javascript/Calculate a card hand/blackjack.js
@@ -0,0 +1,23 @@
+let cardOne = 7;
+let cardTwo = 5;
+let sum = cardOne + cardTwo; // 15
+let cardOneBank = 7;
+let cardTwoBank = 5;
+let cardThreeBank = 6;
+let cardFourBank = 4;
+
+let cardThree = 7;
+sum += cardThree;
+if (sum > 21) {
+    console.log('You lost');
+}
+console.log(`You have ${sum} points`);
+
+let bankSum = cardOneBank + cardTwoBank + cardThreeBank + cardFourBank;
+
+if (bankSum > 21 || (sum <= 21 && sum > bankSum)) {
+    console.log('You win');
+    process.exit(1); // exit program
+} else {
+    console.log('Bank wins');
+}
